029062259d02c7fb462a02838ad8cacb51cf8c59
jframos29 <ramos@gopreki.com>
2020-09-01 17:32:15 -0500

    ramos is an asshole
    Ramos doesn't know what a keyword argument is

--- sftp_client/sftp_client.py
+++ sftp_client/sftp_client.py
@@ -3,6 +3,6 @@ import paramiko
 def get_sftp_client(url, user, password):
     client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    client.connect(url, user, password)
+    client.connect(url, username=user, password=password)
     sftp = client.open_sftp()
     return sftp
